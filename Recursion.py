364ebbddd6597ee964e08e61e1fcbf67f7972b7b
JayChen <Jay_Chennn@outlook.com>
2023-08-07 12:23:38 +0800

    Note Aug 7

--- Recursion.py
+++ Recursion.py
@@ -80,3 +80,35 @@ def count_partitions(n, m):
         with_m = count_partitions(n - m, m)
         without_m = count_partitions(n, m - 1)
         return with_m + without_m
+
+def compose1(f, g):
+    """"Return a function h, such that h(x) = f(g(x))."""
+    def h(x):
+        return f(g(x))
+    return h
+
+def repeated(f, n):
+    """Return the function that computes the nth application of func (recursively!).
+
+    >>> add_three = repeated(lambda x: x + 1, 3)
+    >>> add_three(5)
+    8
+    >>> square = lambda x: x ** 2
+    >>> repeated(square, 2)(5) # square(square(5))
+    625
+    >>> repeated(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    >>> repeated(square, 0)(5)
+    5
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'repeated',
+    ...       ['For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return lambda x: x
+    else:
+        return compose1(f, repeated(f, n - 1))
+    #Don't change the f in the first slot, which is the function that compound g
